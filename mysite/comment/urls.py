be64a85d4d348f749c3668e445774809575756b1
1forij <80508318+1forij@users.noreply.github.com>
2022-12-04 16:19:23 +0800

    Delete mysite directory

--- mysite/comment/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from comment import views
-
-urlpatterns = [
-    path('update_comment',views.update_comment),
-]
--- mysite/read_count/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReadCountConfig(AppConfig):
-    name = 'read_count'
